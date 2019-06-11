3e5a023e32c35299b20adbd05029114ea8eb8f0c
ShirkeJR <jakub.rycyk@outlook.com>
2019-06-11 12:29:00 +0200

    Base commit

---
+++ src/test/java/CinemaSystem/CinemaSystem/CinemaSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package CinemaSystem.CinemaSystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class CinemaSystemApplicationTests {
+
+	@Test
+	void shouldBeTrue() {
+        assertTrue(true);
+	}
+
+}
